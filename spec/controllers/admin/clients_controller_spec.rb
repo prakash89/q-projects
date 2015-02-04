7e27e2901429c820332bfa5096ce652b6c2cf05f
RaviShankarArya <rshankar@qwinixtech.com>
2015-02-04 16:41:47 +0530

    controller spec for clients and link_types

--- spec/controllers/admin/clients_controller_spec.rb
+++ spec/controllers/admin/clients_controller_spec.rb
@@ -4,55 +4,98 @@ require 'spec_helper'
 describe Admin::ClientsController, :type => :controller do
 
   let(:admin) {FactoryGirl.create(:admin_user)}
-  let(:client){FactoryGirl.create(:client)}
   let(:client_1){FactoryGirl.create(:client)}
   let(:client_2){FactoryGirl.create(:client)}
 
+  let(:valid_client_params) { {client: FactoryGirl.build(:client).as_json} }
+  let(:invalid_client_params) { {client: {}} }
 
-  it "POST #create" do
-    client_params = {
-      client: {
-        name: "Client namer",
-        description: "Client Description",
-        city: "city",
-        state: "state",
-        country: "country",
-        pretty_url: "http://www.google.com"
-      }
-    }
-    post :create, client_params, {id: admin.id}
-    expect(Client.count).to  eq 1
-  end
+  context "new" do
+    it "should display the form" do
+      get :new, {}, {id: admin.q_auth_uid}
+      expect(response.status).to eq(200)
 
-  it "GET #index" do
-    [client_1,client_2]
-    get :index, nil, {id: admin.id}
-    expect(assigns[:clients]).to match_array([client_1,client_2])
-  end
-
-  it "GET #show" do
-    get :show, {:id => client.id}, {id: admin.id}
-    expect(assigns(:client)).to eq(client)
-  end
-
-  it "GET #edit" do
-   get :edit, {id: client.id}, {id: admin.id}
-   expect(assigns[:client]).to eq(client)
-  end
-
-  it "PUT #update" do
-    put :update, {:id => client.id, :client => { :name => "New title", :description =>"Tested data",:city =>"city",:state =>"state",:country =>"country",:pretty_url =>"http://www.google.com"}}, {id: admin.id}
-    expect(assigns(:client)).to eq(client)
-  end
-
-  describe "DELETE #destroy" do
-    it "admin should be able to destroy the client" do
-      expect do
-        delete :destroy, {:id => client.to_param}, {id: admin.id}
-        expect(Client.count).to  eq 1
-      end
+      xhr :get, :new, {}, {id: admin.q_auth_uid}
+      expect(assigns(:client)).to be_a Client
     end
   end
 
+  context "create" do
+    it "positive case" do
+      xhr :post, :create, valid_client_params, {id: admin.q_auth_uid}
+      expect(Client.count).to  eq 1
+      expect(response.code).to eq("200")
+    end
+
+    it "negative case" do
+      xhr :post, :create, invalid_client_params, {id: admin.q_auth_uid}
+      expect(Client.count).to  eq 0
+      expect(response.code).to eq("200")
+    end
+  end
+
+  context "edit" do
+    it "should display the form" do
+      get :edit, {id: client_1.id}, {id: admin.q_auth_uid}
+      expect(assigns[:client]).to eq(client_1)
+      expect(response.status).to eq(200)
+
+      xhr :get, :edit, {id: client_1.id}, {id: admin.q_auth_uid}
+      expect(assigns(:client)).to eq(client_1)
+      expect(response.code).to eq("200")
+    end
+  end
+
+  context "update" do
+    it "positive case" do
+      xhr :put, :update, {id: client_1.id, client: client_1.as_json.merge!({"name" =>  "Updated Client Name"})}, {id: admin.q_auth_uid}
+      expect(assigns(:client).errors.any?).to eq(false)
+      expect(assigns(:client).name).to  eq("Updated Client Name")
+      expect(Client.count).to  eq 1
+      expect(response.code).to eq("200")
+    end
+
+    it "negative case" do
+      xhr :put, :update, {id: client_1.id, client: client_1.as_json.merge!({"name" =>  ""})}, {id: admin.q_auth_uid}
+      expect(assigns(:client).errors.any?).to eq(true)
+      expect(Client.count).to  eq 1
+      expect(response.code).to eq("200")
+    end
+  end
+
+  context "index" do
+    it "should return the clients details" do
+      [client_1, client_2]
+      get :index, {}, {id: admin.q_auth_uid}
+      expect(assigns[:clients]).to match_array([client_1,client_2])
+      expect(response.status).to eq(200)
+
+      xhr :get, :index, {}, {id: admin.q_auth_uid}
+      expect(assigns[:clients]).to match_array([client_1,client_2])
+      expect(response.code).to eq("200")
+    end
+  end
+
+  context "show" do
+    it "should return the specific client" do
+      get :show, {:id => client_1.id}, {id: admin.q_auth_uid}
+      expect(assigns[:client]).to eq(client_1)
+      expect(assigns[:clients]).to match_array([client_1,client_2])
+      expect(response.status).to eq(200)
+
+      xhr :get, :show, {id: client_1.id}, {id: admin.q_auth_uid}
+      expect(assigns[:client]).to eq(client_1)
+      expect(response.code).to eq("200")
+    end
+  end
+
+  context "destroy" do
+    it "should remove the client" do
+      [client_1, client_2]
+      xhr :delete, :destroy, {id: client_1.id}, {id: admin.q_auth_uid}
+      expect(Client.count).to  eq 1
+      expect(response.code).to eq("200")
+    end
+  end
 
 end
--- spec/controllers/admin/link_types_controller_spec.rb
+++ spec/controllers/admin/link_types_controller_spec.rb
@@ -4,54 +4,98 @@ require 'spec_helper'
 describe Admin::LinkTypesController, :type => :controller do
 
   let(:admin) {FactoryGirl.create(:admin_user)}
-  let(:link_type){FactoryGirl.create(:link_type)}
   let(:link_type_1){FactoryGirl.create(:link_type)}
   let(:link_type_2){FactoryGirl.create(:link_type)}
 
+  let(:valid_link_type_params) { {link_type: FactoryGirl.build(:link_type).as_json} }
+  let(:invalid_link_type_params) { {link_type: {}} }
 
-  it "POST #create" do
-    link_type_params = {
-      link_type: {
-        name: "Client namer",
-        description: "Client Description",
-        button_text: "button_text"
-      }
-    }
-    post :create, link_type_params, {id: admin.id}
-    expect(LinkType.count).to  eq 1
-  end
 
-  it "GET #index" do
-    [link_type_1,link_type_2]
-    get :index, nil, {id: admin.id}
-    expect(assigns[:link_types]).to match_array([link_type_1,link_type_2])
-  end
+  context "new" do
+    it "should display the form" do
+      get :new, {}, {id: admin.q_auth_uid}
+      expect(response.status).to eq(200)
 
-  it "GET #show" do
-    get :show, {:id => link_type.id}, {id: admin.id}
-    expect(assigns(:link_type)).to eq(link_type)
-  end
-
-  it "GET #edit" do
-   get :edit, {id: link_type.id}, {id: admin.id}
-   expect(assigns[:link_type]).to eq(link_type)
-  end
-
-  it "PUT #update" do
-    put :update, {:id => link_type.id, :link_type => { :name => "Updated title", :description =>"Tested data",:city =>"city"}}, {id: admin.id}
-    expect(assigns(:link_type)).to eq(link_type)
-  end
-
-  describe "DELETE #destroy" do
-    it "admin should be able to destroy the link_type" do
-      expect do
-        delete :destroy, {:id => link_type.to_param}, {id: admin.id}
-        expect(LinkType.count).to  eq 1
-      end
+      xhr :get, :new, {}, {id: admin.q_auth_uid}
+      expect(assigns(:link_type)).to be_a LinkType
     end
   end
 
+  context "create" do
+    it "positive case" do
+      xhr :post, :create, valid_link_type_params, {id: admin.q_auth_uid}
+      expect(LinkType.count).to  eq 1
+      expect(response.code).to eq("200")
+    end
 
+    it "negative case" do
+      xhr :post, :create, invalid_link_type_params, {id: admin.q_auth_uid}
+      expect(LinkType.count).to  eq 0
+      expect(response.code).to eq("200")
+    end
+  end
 
+  context "edit" do
+    it "should display the form" do
+      get :edit, {id: link_type_1.id}, {id: admin.q_auth_uid}
+      expect(assigns[:link_type]).to eq(link_type_1)
+      expect(response.status).to eq(200)
 
+      xhr :get, :edit, {id: link_type_1.id}, {id: admin.q_auth_uid}
+      expect(assigns(:link_type)).to eq(link_type_1)
+      expect(response.code).to eq("200")
+    end
+  end
+
+  context "update" do
+    it "positive case" do
+      xhr :put, :update, {id: link_type_1.id, link_type: link_type_1.as_json.merge!({"name" =>  "Updated link_type Name"})}, {id: admin.q_auth_uid}
+      expect(assigns(:link_type).errors.any?).to eq(false)
+      expect(assigns(:link_type).name).to  eq("Updated link_type Name")
+      expect(LinkType.count).to  eq 1
+      expect(response.code).to eq("200")
+    end
+
+    it "negative case" do
+      xhr :put, :update, {id: link_type_1.id, link_type: link_type_1.as_json.merge!({"name" =>  ""})}, {id: admin.q_auth_uid}
+      expect(assigns(:link_type).errors.any?).to eq(true)
+      expect(LinkType.count).to  eq 1
+      expect(response.code).to eq("200")
+    end
+  end
+
+  context "index" do
+    it "should return the link type details" do
+      [link_type_1, link_type_2]
+      get :index, {}, {id: admin.q_auth_uid}
+      expect(assigns[:link_types]).to match_array([link_type_1,link_type_2])
+      expect(response.status).to eq(200)
+
+      xhr :get, :index, {}, {id: admin.q_auth_uid}
+      expect(assigns[:link_types]).to match_array([link_type_1,link_type_2])
+      expect(response.code).to eq("200")
+    end
+  end
+
+  context "show" do
+    it "should return the specific link type" do
+      get :show, {:id => link_type_1.id}, {id: admin.q_auth_uid}
+      expect(assigns[:link_type]).to eq(link_type_1)
+      expect(assigns[:link_types]).to match_array([link_type_1,link_type_2])
+      expect(response.status).to eq(200)
+
+      xhr :get, :show, {id: link_type_1.id}, {id: admin.q_auth_uid}
+      expect(assigns[:link_type]).to eq(link_type_1)
+      expect(response.code).to eq("200")
+    end
+  end
+
+  context "destroy" do
+    it "should remove the link type" do
+      [link_type_1, link_type_2]
+      xhr :delete, :destroy, {id: link_type_1.id}, {id: admin.q_auth_uid}
+      expect(LinkType.count).to  eq 1
+      expect(response.code).to eq("200")
+    end
+  end
 end
